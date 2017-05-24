06cfdda418f163d45409b2777aec205e5402b983
bharathappali <bharathcoolon@gmail.com>
2017-05-24 15:19:43 +0530

    Changed required things in spec files

--- spec/java_buildpack/jre/ibm_jre_initializer_spec.rb
+++ spec/java_buildpack/jre/ibm_jre_initializer_spec.rb
@@ -18,14 +18,14 @@
 require 'spec_helper'
 require 'component_helper'
 require 'java_buildpack/component/mutable_java_home'
-require 'java_buildpack/jre/ibmjre_like'
+require 'java_buildpack/jre/ibm_jre_initializer'
 
-describe JavaBuildpack::Jre::Ibmjava do
+describe JavaBuildpack::Jre::IbmJreInitializer do
   include_context 'component_helper'
   let(:java_home) { JavaBuildpack::Component::MutableJavaHome.new }
 
-  it 'detects with id of ibmjre_like-<version>' do
-    expect(component.detect).to eq("ibmjre-like=#{version}")
+  it 'detects with id of ibm_jre_initializer-<version>' do
+    expect(component.detect).to eq("ibm-jre-initializer=#{version}")
   end
   it 'installs the java from bin', cache_fixture: 'stub-java.bin' do
     component.detect
